79debf4642b48751f5c2734193346c7117d030ad
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-02-13 09:47:45 -0700

    created constructor for creating cards objects

--- BlackJack.js
+++ BlackJack.js
@@ -1,13 +1,34 @@
-// function to create a deck of cards
 let deck = [];
 let newDeck = [];
+let playerHand = [];
+let playerPoints = 0;
+
+//point system: cards 2-9 will count as their corresponding values. 
+//Cards 10, jack, queen, and king will all count as 10 points. 
+//The ace card can count as 1 or 10.
+//21 points wins the game. Over 21 busts.
+
+
+
+
+//constructor for creating array of objects representing cards in deck
+class Card {
+  constructor(ranks, suits) {
+    this.ranks = ranks; 
+    this.suits = suits;
+    this.value = 0;
+  }
+}
+
+
+// function to create a deck of cards
 function creatDeck() {
-  let suits = ['H', 'C', 'D', 'S'];
-  let ranks = ['2', '3', '4', '5','6', '7', '8', '9', 'T', 'J','Q', 'K', 'A'];
-    
+  const suits = ["Heart's", "Club's", "Diamond's", "Spade's"];
+  const ranks = ['2', '3', '4', '5','6', '7', '8', '9', '10', "Jack" ,"Queen", "King", "Ace"];
+  
   for (let i = 0; i < suits.length; i++) {
     for (let j = 0; j < ranks.length; j++) {
-      deck.push(suits[i] + ranks[j]);
+      deck.push(new Card(ranks[j], suits[i]));
     }
   }
   return deck;    
@@ -25,8 +46,18 @@ function shuffleDeck(deck) {
   return newDeck;
   
 }
+//get values for cards
+
+// function assignCardValues(card) {
+//   switch (card.rank)
+// }
+
+//switch to assign value of cards.
+
+
 
 
 creatDeck();
+
 shuffleDeck(deck);
 console.log(newDeck);
